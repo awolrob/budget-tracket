68957a8f2872c3370e033d651daff817d78045d8
Rob Ellingson <76190447+awolrob@users.noreply.github.com>
2021-10-02 10:28:01 -0400

    PWA

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 
 * use IndexedDB to add offline functionality. Review 
 `Module 18`: NoSQL, `Lesson 4`: Add Offline Persistence with IndexedDB as a refresher on how to add this to your application.
+- Done!
 
 * add a service worker to your application. Review 
 `Module 19`: Progressive Web Applications (PWA), `Lesson 4`: Using Service Workers as a refresher on how to add this to your application.
+- Done!
 
 * you’ll need to deploy this application to Heroku using MongoDB Atlas. To review this process, look at 
 `Module 18`: NoSQL, `Lesson 5`: Add Mongoose Validation, specifically 18.5.5: Deploy to Heroku.
 
 * manually and add `manifest.json` to the root of the public/ directory of your application. You can also review 
 `Module 19`: Progressive Web Applications (PWA), `Lesson 5`: Convert the App to a PWA as a refresher on web manifests.
+- Done!
 
 Express.js server provided to you in the starter code.
 
---
+++ public/service-worker.js
@@ -0,0 +1,69 @@
+const APP_PREFIX = 'BudgetTracket-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./js/idb.js",
+    "./js/index.js",
+    "./css/styles.css",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
+    "http://localhost:3001/api/transaction"
+];
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+        })
+    )
+})
+
+// Cache
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // `keyList` contains all cache names under your username.github.io
+            // filter out ones that has this app prefix to create keeplist
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    );
+});
